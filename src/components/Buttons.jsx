d2708b888db4ef095e02a27019de02ea151b3c58
ammar <ammar.botonjic@convergent-usa.com>
2019-12-28 16:09:19 +0100

    New button component

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Buttons extends Component {
+    render() {
+        return (
+            <div>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        );
+    }
+}
+
+export default Buttons;
--- src/components/PersonsList.jsx
+++ src/components/PersonsList.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import Buttons from './Buttons';
+import '../App.css'
 
 const PersonsList = (props) => {
     const { persons } = props;
 
     const mapPersons = () => {
-        return persons.map(person => <li key={person.id}>{person.name}</li>)
+        return persons.map(person => 
+            <div key={person.id}>
+                <li key={person.id}>
+                {person.name}
+            </li>
+
+            <Buttons persons={persons} />
+            </div>
+        )
     }
 
     return (
         <section>
-            <ul>
+            <ul className="user-list">
                 { mapPersons() }
             </ul>
         </section>
